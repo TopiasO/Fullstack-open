f1493e680a9663be3b5f960170eb94683cbacd98
Topias Ojanen <topiasojanen@gmail.com>
2024-10-24 18:10:55 +0300

    Added exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import sameName from './components/SameName'
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
@@ -6,17 +8,22 @@ import PersonsToShow from './components/PersonsToShow'
  
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
 
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }
+
+  useEffect(hook, [])
+  console.log('render', persons.length, 'persons')
+ 
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
